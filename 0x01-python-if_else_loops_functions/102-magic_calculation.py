8592909c50cd443aae53dae1f78769ff75e200b7
titikorie <titikorie@gmail.com>
2023-06-06 19:53:30 -0700

    done

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+# Author: Titilayo Korie
+
 def magic_calculation(a, b, c):
-	 """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
     if c > b:
