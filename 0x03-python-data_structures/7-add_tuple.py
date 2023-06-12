023d8443372f772bcd7b44f638076243c0aeadf2
titikorie <titikorie@gmail.com>
2023-06-12 07:35:41 -0700

    done

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-add_tuple = __import__('7-add_tuple').add_tuple
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
