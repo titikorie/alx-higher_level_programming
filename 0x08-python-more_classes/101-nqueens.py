3388e813dbb203f80cf2bdb12342271f6d207a2d
titikorie <titikorie@gmail.com>
2023-07-05 16:18:45 -0700

    done

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,47 @@
 #!/usr/bin/python3
 """
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
 """
 
 
-from sys import argv
+if __name__ is "__main__":
+    import sys
+def main()
+    """ """
+    args = sys.argv
+    if len(args) is not 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
 
-if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-    n = int(argv[1])
+    n = args[1]
+    # n must be integer >= 4
+    if type(n) is not int:
+        print('N must be a number')
+        sys.exit(1)
     if n < 4:
-        print("N must be at least 4")
-        exit(1)
+        print('N must be at least 4')
+        sys.exti(1)
 
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
+    # create board
+    board = [[0 for col in range(n)] for row in range(n)]
 
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
+    for row in range(n):  # each possible placement
+        for col in range(n):
+            placed = 0
+            while placed < n:
+            
+                if check_placement(board, [row, col]) is True:
+                    # place queen
+                    placed += 1
 
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
-            return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
-                return False
-            i += 1
-        return True
+            print_board # print board
+    return 0
 
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
+def check_placement(board, pos)
+    """Determine if the queen can safely assume position"""
 
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+    # check column
 
-    # start the recursive process at x = 0
-    nqueens(0)
+    # check diagnols
+
+def print_board()
+    """Prints board with queens safely positioned"""
