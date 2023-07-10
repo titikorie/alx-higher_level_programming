5ddeaf5cd5c6cf45a921f47e8af202b7d3879f11
titikorie <titikorie@gmail.com>
2023-07-10 16:50:09 -0700

    done

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,17 +3,15 @@
 
 
 class MyInt(int):
-    """Redefine equality comparisons"""
-    def __eq__(self, other_int):
-        """Return equal if not equal"""
-        if self is not other_int:
-            return True
-        else:
+    """weird class"""
+    def __eq__(self, other):
+        """== method"""
+        if int.__eq__(self, other):
             return False
+        return True
 
-    def __ne__(self, other_int):
-        """Return not equal if equal"""
-        if self is other_int:
-            return True
-        else:
+    def __ne__(self, other):
+        """!= method"""
+        if int.__ne__(self, other):
             return False
+        return True
